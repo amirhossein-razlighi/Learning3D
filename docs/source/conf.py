88fdca26e9dd9acf7cb969ef774e419c7dac9cf6
AmirHossein-nr <arazlighi@gmail.com>
2025-02-20 21:45:56 +0330

    add workflow for doc deployment

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,10 +19,11 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.viewcode',
     'sphinx.ext.intersphinx',
+    'sphinx_rtd_theme',
 ]
 
 html_theme = 'sphinx_rtd_theme'
-
+html_static_path = ['_static']
 templates_path = ['_templates']
 exclude_patterns = []
 
@@ -30,6 +31,3 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
