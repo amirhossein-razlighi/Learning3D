30dd0f25562f3b92a221202b85140df04a3dbc58
AmirHossein-nr <arazlighi@gmail.com>
2025-02-21 15:07:55 +0330

    move sample script for 360 rendering

---
+++ scripts/render_a_mesh_360.py
@@ -0,0 +1,23 @@
+import click
+from pathlib import Path
+import torch
+from torch3dr.IO import load_external_mesh
+from torch3dr.Rendering import render_360
+
+
+@click.command()
+@click.option("--mesh_path", type=str, required=True)
+@click.option("--output_path", type=str, required=False, default="")
+@click.option("--verbose", is_flag=True)
+def main(mesh_path: str, output_path: str, verbose: bool):
+    mesh_path = Path(mesh_path)
+    vertices, faces = load_external_mesh(mesh_path)
+    vertices = vertices.unsqueeze(0)
+    faces = faces.unsqueeze(0)
+    textures = torch.ones_like(vertices)
+    output_path = output_path if output_path != "" else "obj_360_rendered.gif"
+    render_360(vertices, faces, textures, output_path=output_path, verbose=verbose)
+
+
+if __name__ == "__main__":
+    main()
--- torch3dr/Rendering/render_360.py
+++ torch3dr/Rendering/render_360.py
@@ -94,21 +94,3 @@ def render_360(
     imageio.mimsave(output_path, images, duration=1000 / fps, loop=0)
 
     return output_path
-
-
-@click.command()
-@click.option("--mesh_path", type=str, required=True)
-@click.option("--output_path", type=str, required=False, default="")
-@click.option("--verbose", is_flag=True)
-def main(mesh_path: str, output_path: str, verbose: bool):
-    mesh_path = Path(mesh_path)
-    vertices, faces = load_external_mesh(mesh_path)
-    vertices = vertices.unsqueeze(0)
-    faces = faces.unsqueeze(0)
-    textures = torch.ones_like(vertices)
-    output_path = output_path if output_path != "" else "cow_obj_360_rendered.gif"
-    render_360(vertices, faces, textures, output_path=output_path, verbose=verbose)
-
-
-if __name__ == "__main__":
-    main()
